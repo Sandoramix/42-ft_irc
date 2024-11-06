cfd8e52326c8f6df731a81f05c3566d62cb9d181
gbarone42 <badph3@gmail.com>
2024-11-06 15:07:47 +0100

    fix: NICK validation order. Added `settings.json`

--- src/cmd/NickCmd.cpp
+++ src/cmd/NickCmd.cpp
@@ -22,12 +22,15 @@ void NickCmd::run(Client& requestedFrom, const std::vector<std::string>& params)
 		return;
 	}
 	const std::string& oldNickname = requestedFrom.getNickname();
-	requestedFrom.setNickname(params[0]);
 	Client* foundClient = this->server.findClientByNickname(newNickname);
 	if (foundClient != NULL && foundClient->getSocketFd() != requestedFrom.getSocketFd()) {
 		debugError("Client[" << requestedFrom.getSocketFd() << "] tried to change nickname to already used nickname");
 		requestedFrom.sendMessage(ResponseMsg::genericResponse(ERR_NICKNAMEINUSE, requestedFrom.getNickname()));
 		return;
 	}
+	if (foundClient){
+		debugInfo("Found client [" << foundClient->getSocketFd() << "] Nickname: " << foundClient->getNickname());
+	}
+	requestedFrom.setNickname(params[0]);
 	this->server.notifyClientOfNicknameChange(requestedFrom, oldNickname);
 }
